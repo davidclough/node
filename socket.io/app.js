5785278ccd198fe28e23a24d0f193ff96b552d75
David Clough <david.clough@3squared.com>
2016-08-07 18:38:20 +0100

    Enable some username functionality.

--- socket.io/app.js
+++ socket.io/app.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 
+var usernames = [];
+
 // Allow "non-routed" access to folders.
 //app.use(express.static('.'));
 app.use('/public', express.static('public'));
@@ -14,10 +16,35 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "\\index.html")
 });
 
+var updateUsernames = function () {
+  io.sockets.emit("usernames", usernames);         // including me
+};
+
 // This like a "socket ready" handler.
 io.sockets.on("connection", function (socket) {
+  socket.on("new user", function (data, callback) {
+    if (usernames.indexOf(data) !== -1) {
+      callback(false);
+      //callback({ isValid:false });
+    } else {
+      callback(true);
+      socket.name = data;
+      usernames.push(socket.name)
+      updateUsernames();
+    }
+  });
+
+  // Here "send message" is a custom event??? Iassume it corresponds to the emit in client-side.
   socket.on("send message", function (data) {
     io.sockets.emit("new message", data);         // including me
     //socket.broadcast.emit("new message", data);   // excluding me
+  });
+
+  socket.on("disconnect", function (data) {
+    var userIndex;
+    if (socket.name && (userIndex = usernames.indexOf(socket.name) >= 0)) {
+      usernames.splice(userIndex, 1);
+      updateUsernames();
+    }
   });
 });
--- socket.io/public/client.js
+++ socket.io/public/client.js
@@ -1,8 +1,43 @@
 $(function () {
   var socket = io.connect();
+
+  var nameForm = $("#nameForm");
+  var nameError = $("#nameError");
+  var nameBox = $("#nameBox");
+
   var messageForm = $("#send-message");
   var messageBox = $("#message");
   var chat = $("#chat");
+
+
+  var sendName = function () {
+    var name = nameBox.val();
+    if (name !== "") {
+      // emit with callback.
+      socket.emit("new user", name, function (data) {
+        if (data) {
+          $("#nameWrap").slideUp();
+          $("#contentWrap").slideDown();
+        } else {
+          nameError.html("Name already taken")
+        }
+      });
+      nameBox.val("");
+    }
+  };
+
+  nameForm.submit(function (e) {
+    e.preventDefault();
+    sendName();
+  });
+
+  socket.on("usernames", function (data) {
+    $("#userList").html("");
+    $.each(data, function(index, value) {
+      $("#userList").append("<li>" + value +"</li>");
+    });
+  });
+
 
   var sendNonEmptyMessage = function () {
     var message = messageBox.val();
