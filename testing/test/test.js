dfda1381211e27a48dd90c82c058fde4c4dc986e
David Clough <david.clough@3squared.com>
2016-08-09 19:50:43 +0100

    Added a "testing" folder for tests using karma and jasmine.

---
+++ testing/test/test.js
@@ -0,0 +1,13 @@
+describe("A test", function () {
+  it ("is true", function () {
+    var test = true;
+    expect(test).toBe(false);
+  });
+});
+
+describe("Another BBBBBBBBBBBBBBBBBBBBB test", function () {
+  it ("is true aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", function () {
+    var test = true;
+    expect(test).toBe(false);
+  });
+});
