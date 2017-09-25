fd16f37ee15aeb04c33ec8708428d377d765df77
David Clough <david.clough@3squared.com>
2017-09-25 20:27:21 +0100

    Completed recaptchanode.

--- recaptchanode/server.js
+++ recaptchanode/server.js
@@ -7,11 +7,34 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/subscribe", (req, res) => {
+  if (!req.body.captcha) {
+    return res.json({ "success": false, "msg": "Please select captcha" });
+  }
+
+  const secretKey = "6LdaCDIUAAAAAOVAK2nEtmER5YWd1CV5y8sYQcMQ";
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+  request(verifyUrl, (err, response, body) => {
+    body = JSON.parse(body);
+
+    // NOTE: This gets logged to the console window where the node is being run from. Only client-side code in index.html will display in browser console.
+    console.log(body);
+
+    if (body.success !== undefined && !body.success) {
+      return res.json({ "success": false, "msg": "Failed captcha verification" });
+    }
+    return res.json({ "success": true, "msg": "Captcha passed" });
+  });
+});
+
 app.listen(3000, () => {
   console.log("server sarted on port 3000");
 })
 
-// node server
+// node server              because this file is called server.js
+// nodemon
