39a345289044427cde501c9bd6b7f4a6b2bf6a1f
David Clough <david.clough@3squared.com>
2017-10-12 18:48:04 +0100

    Moved state definition from the store into a different module.

--- vue/vuex/vuex-videos/walk-through/src/store/store.js
+++ vue/vuex/vuex-videos/walk-through/src/store/store.js
@@ -1,22 +1,16 @@
 import Vue from "vue"
 import Vuex from "vuex"
 
-import actions from "./actions"       // . is relative to current file.
+import state from "./state"       // . is relative to current file.
+import actions from "./actions"
 import getters from "./getters"
 import mutations from "./mutations"
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  state: {
-    registrations: [],
-    users: [
-      {id: 1, name: 'Max', registered: false},
-      {id: 2, name: 'Anna', registered: false},
-      {id: 3, name: 'Chris', registered: false},
-      {id: 4, name: 'Sven', registered: false}
-    ]
-  },
+  state,
+  
   // 2nd Video.
   getters,
   mutations,
