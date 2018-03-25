26001de3b52a1aa0707086d9ad2afb22e3ea232b
David Clough <david.clough@3squared.com>
2018-03-25 16:56:06 +0100

    Add webpack-4-test folder containing 2 files edited when tried upgrading from webpack v1 to v4.
    A number of errors occurred when ran "npm start". Although the webpack.config.dev.js was adjusted so we could actually build the code, the page did not load. It is unlikely to be a good idea to try to upgrade all the packages from within this project. We should probably find a more modern template.

---
+++ react-redux-1/webpack-4-test/webpack.config.dev.js
@@ -0,0 +1,66 @@
+import webpack from 'webpack';
+import path from 'path';
+
+// ****************** check that no linting error is preventing compilation
+
+export default {
+  // debug: true,     // Moved to new LoaderOptionsPlugin entry when upgraded to webpack 4.
+
+  // NOTE: In his example debugger WORKS.
+
+  //devtool: 'cheap-module-eval-source-map',
+  // source-map DID work for me - the debugger statements broke at the line I put them on.
+  //devtool: 'source-map',        // Worked.
+  //devtool: 'eval-source-map',     // Worked.
+  devtool: 'cheap-module-source-map',     // XXDidn't work. AT LATER DATE - it worked fine.
+
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    //path.resolve(__dirname, 'src/index')
+
+    // Important that the app entry point is defined last.
+    // DC: This is referring index.js and not the html file.
+    //     It is the server file, e.g. srcServer.js, which sends out the index.html.
+    './src/index'
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    noInfo: false,
+    //contentBase: path.resolve(__dirname, 'src')
+    contentBase: './src'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    })
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      // Bootstrap file types for fonts.
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  },
+
+
+  // DC: https://moduscreate.com/blog/es6-es2015-import-no-relative-path-webpack/
+  resolve: {
+    modules: [     // We are using WebPack 1 in this project, NOT 2.
+    // root: [
+      path.resolve('./node_modules'),
+      path.resolve('./src')
+    ]
+  }
+};
